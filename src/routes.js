d80ce4d752eb2a016362ca9b78f6b2cf8b1caf12
Ravioli <atran160@mtroyal.ca>
2020-11-12 19:41:34 -0800

    changes and test for google cloud

--- src/routes.js
+++ src/routes.js
@@ -3,67 +3,81 @@
 module.exports.register = (app, database) => {
 
     app.get('/', async (req, res) => {
-        res.status(200).send("You did it! I am now running:) ").end();
+        res.status(200).send("You did it! I am now running:) this codfe works ").end();
     });
 
+
+    app.get('/courses', async (req, res) => {
+        console.log("=================");
+        let query;
+        const query = database.query(
+            'select * from courses ;'
+        );
+        
+        console.log(query);
+        const courses = await query;
+
+        res.status(200).send(JSON.stringify(course)).end();
+        });
+    /*
     //Gets all courses or specific course with the name
-    app.get('/api/courses', async (req, res) => {
+    app.get('/courses', async (req, res) => {
         console.log("=================");
         let query;
         if (req.query.name) {
             let _name = req.query.name;
             query = database.query(
-                'select * from course_list where name = ?',
+                'select * from courses where course_name = ?;',
                 [_name]
             );
         } else {
             query = database.query(
-                'SELECT * FROM course_list'
+                'SELECT * FROM courses;'
             );
         }
         console.log(query);
-        const emps = await query;
+        const courses = await query;
 
-        res.status(200).send(JSON.stringify(emps)).end();
+        res.status(200).send(JSON.stringify(courses)).end();
     });
 
 
     //Gets courses with a specific course id
-    app.get('/api/courses/:id', async (req, res) => {
+    app.get('/courses/:id', async (req, res) => {
         let _id = req.params.course_id;
         const query = database.query(
-            'select * from course_list where id = ?',
+            'select * from courses where course_id = ?;',
             [_id]
         );
-        const emps = await query;
-        res.status(200).send(JSON.stringify(emps)).end();
+        const courses = await query;
+        res.status(200).send(JSON.stringify(courses)).end();
     });
 
 
     //Adds new courses
-    app.post('/api/courses', async (req, res) => {
+    app.post('/courses', async (req, res) => {
         let _courseid = req.body.course_id;
         let _name = req.body.name;
         let _description = req.body.description;
 
         const query = database.query(
-            'insert into course_list(course_id, name, description) values (?, ?, ?)',
+            'insert into courses(course_id, course_name, description) values (?, ?, ?);',
             [_courseid, _name, _description]
         );
-        const emps = await query;
+        const courses = await query;
         res.status(200).send('Course added successfully!').end();
     });
 
     //Updates the description
-    app.post('/api/courses', async (req, res) => {
+    app.post('/courses', async (req, res) => {
         let _description = req.body.description;
 
         const query = database.query(
-            'update course_list set description = ?',
+            'update courses set description = ?;',
             [_description]
         );
-        const emps = await query;
+        const courses = await query;
         res.status(200).send('Course added successfully!').end();
     });
-
+*/
 };
